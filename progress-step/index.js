43a0bb066447faaac06d8050f700d826a78f8ab7
高永昭 <498763684@qq.com>
2022-06-19 17:56:26 +0800

    progress-step

--- progress-step/index.js
+++ progress-step/index.js
@@ -5,29 +5,39 @@ window.addEventListener("load", () => {
   const others = document.querySelectorAll(".other");
   const span = document.querySelectorAll("span");
   const em = document.querySelectorAll("em");
+  // 定义全局变量控制样式添加
+  let num = 0;
+  btnDisabled()
   // 点击那个按钮那个按钮亮起
-  let num = others.length;
   left.addEventListener("click", () => {
-    right.classList.remove("active");
-    left.classList.add("active");
-    // 定义全局变量控制样式添加
     if (num > 0) {
       num--;
       span[num].style = "transition: all .4s";
       span[num].classList.remove("bgc");
       em[num].style = "transition: all .4s";
       em[num].classList.remove("actives");
-    } else if (num === 0) {
+    } else if(num == 0) {
       num = 0;
     }
+    btnDisabled()
   });
   right.addEventListener("click", () => {
-    left.classList.remove("active");
-    right.classList.add("active");
     if (num < others.length) {
       span[num].classList.add("bgc");
       em[num].classList.add("actives");
       num++;
     }
+    btnDisabled()
   });
+
+  function btnDisabled() {
+    if(num===0) {
+      left.disabled = true
+    } else if (num === others.length) {
+      right.disabled = true
+    } else  {
+     right.disabled = false
+     left.disabled = false
+    }
+  }
 });
